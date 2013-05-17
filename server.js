19ccd7542a09d7f3bc496188c4ded94a8e97c6c3
andersmartini <andersmartini2@gmail.com>
2013-05-17 00:54:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var url = require("url")
 
 function start(route) {
   function onRequest(request, response) {
-  	var pathname = url.parse(request.url).pathname;
+  	var pathName = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
 
 	route(pathname);
