e835e057fcc74e2a2631100351ebc5623a126aa3
Karl Böhlmark <karl.bohlmark@edgeware.tv>
2013-05-24 18:25:27 +0200

    funkar initial

--- router.js
+++ router.js
@@ -1,5 +1,14 @@
-function route(pathname){
-	console.log("about to route a request for" + pathname);
+
+function route(handle,pathName,response,request){
+	console.log("about to route a request for" + pathName)
+	if(typeof handle[pathName] === "function"){
+		handle[pathName](response, request);
+	} else {
+		console.log("No requesthandler found for " + pathName);
+		response.writeHead(404, {"Content-Type" : "text/plain"});
+		response.write("404 ta bebado?")
+		response.end();	
+	}
 }
 
 exports.route = route;
